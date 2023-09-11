ee504a6edf856bc47f1fa0e919857fc0db799d8c
lviet <lviet456@gmail.com>
2023-09-11 09:48:51 +0700

    [SPLASH_SCREEN] add animated

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,9 +1,41 @@
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Animated} from 'react-native';
 import { Colors } from '../constants/styles';
+import { useEffect, useRef } from 'react';
 const SplashScreen = () => {
+
+    const flickerAnimation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(flickerAnimation, {
+                    toValue: 1,
+                    duration: 1000,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(flickerAnimation, {
+                    toValue: 0,
+                    duration: 1000,
+                    useNativeDriver: true,
+                }),
+            ]),
+        ).start();
+    }, [flickerAnimation]);
+
+    const opacity = flickerAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0.5, 1],
+    });
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>UTE Locker</Text>
+            <Animated.View
+                style={{
+                    opacity: opacity,
+                }}
+            >
+                <Text style={styles.text}>UTE Locker</Text>
+            </Animated.View>
         </View>
     );
 };

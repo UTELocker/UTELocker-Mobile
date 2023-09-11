8d116746b110f917a2027f6157e92d2cee21048c
lviet <lviet456@gmail.com>
2023-09-11 09:49:35 +0700

    [CHORE] remove dont use and add logo Dropdown Component

--- src/components/Auth/DropdownComponent.js
+++ src/components/Auth/DropdownComponent.js
@@ -4,22 +4,12 @@ import { Dropdown } from 'react-native-element-dropdown';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Colors } from '../../constants/styles';
 
-const data = [
-  { label: 'Item 1', value: '1' },
-  { label: 'Item 2', value: '2' },
-  { label: 'Item 3', value: '3' },
-  { label: 'Item 4', value: '4' },
-  { label: 'Item 5', value: '5' },
-  { label: 'Item 6', value: '6' },
-  { label: 'Item 7', value: '7' },
-  { label: 'Item 8', value: '8' },
-];
-
 const DropdownComponent = ({
   placeholder,
   list = [],
   setValue,
-  value
+  value,
+  logo
 }) => {
   
   const renderItem = item => {
@@ -65,7 +55,7 @@ const DropdownComponent = ({
           setValue(item.value);
         }}
         renderLeftIcon={() => (
-          <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+          logo === undefined ? <AntDesign style={styles.icon} color="black" name="Safety" size={20} /> : logo
         )}
         renderItem={renderItem}
       />
